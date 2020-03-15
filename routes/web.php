721f238b653644e11023a62c0a210e69efa93cbf
Taylor Fahey <fahey085@gmail.com>
2020-03-15 12:21:39 -0700

    add vue to laravel project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'freeTrial@displayForm')->name('displayForm');
+Route::post('submitForm', 'freeTrial@submit')->name('submit');
